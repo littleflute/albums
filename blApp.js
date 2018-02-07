395d1106826db8d8204eeb4fcb96a1df159add3c
littleflute <13699175164@sina.cn>
2018-02-08 03:47:00 +0800

    Update blApp.js

--- blApp.js
+++ blApp.js
@@ -13,13 +13,13 @@ function _on_off_div(b,d){
 
 function _blAlbumClass(_v,_list)
 { 
-	this.blhFun1 = function(_o){
+	this.blhFun1 = function(_o,_txt,_x,_y,_w,_h,_color){
 		var d = _o;
-		d.b = blo0.blBtn(d,d.id + "_b","AlbumInf" ,blGrey[i]);
+		d.b = blo0.blBtn(d,d.id + "_b",_txt ,blGrey[i]);
 		d.b.onclick = function(_d, _album){
 			return function(){
 				if(!_d.v){
-					_d.v = blo0.blMDiv(document.body,_d.id + "_v",_album.title,111,111,500,400,"brown");
+					_d.v = blo0.blMDiv(document.body,_d.id + "_v",_album.title,_x,_y,_w,_h,_color);
 					_d.v.d = blo0.blDiv(_d.v,_d.v.id + _album.title,_album.url ,"grey");
 					blo0.blShowObj2Div(_d.v.d, _album);					 
 				}	
@@ -29,12 +29,12 @@ function _blAlbumClass(_v,_list)
 	}
 	for(i in _list){
 		var d = blo0.blDiv(_v,_v.id + "_album" + i,_list[i].title ,blColor[i]);
-		this.blhFun1(d);
+		this.blhFun1(d,"fun1",50*i,50,500,400,blColor[i]);
 	}
 }
 function _blJobClass(p)
 { 
-	this.v = "v0.0.211"; 
+	this.v = "v0.0.212"; 
 	this.blrShowList = function(b , d){
 		
 		if(!d.v){
